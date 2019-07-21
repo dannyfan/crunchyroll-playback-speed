225b179164536e8a7e7f4b4bee6a79eba302497e
Danny Fan <dannyfan@live.com>
2019-07-21 03:21:16 -0400

    Fix range input to trigger video speed

--- playback.js
+++ playback.js
@@ -1,30 +1,18 @@
-function createVideoReference() {
-    if (document.querySelector('crunchroll-video-player-ref')) {
-        return false;
-    }
-    let s = document.createElement('script');
-    s.type = 'type/javascript';
-    s.id = 'crunchyroll-video-player-ref';
-    s.innerHTML = `var videoPlayerObj;`;
-    document.body.appendChild(s);
-}
-
-function updatePlaybackRate(playbackValue) {
-    let videoPlayer = document.querySelector('#player_html5_api');
+function updatePlaybackRate(videoPlayer, playbackValue) {
     if (videoPlayer !== null) {
         setTimeout(() => {
-            console.log(playbackValue);
             videoPlayer.playbackRate = playbackValue;
         }, 5000);
     } 
 }
 
 window.onload = function() {
+    let videoPlayer = document.querySelector('#player_html5_api');
     chrome.storage.sync.get({number: 2}, function(items) {
-        updatePlaybackRate(items.number);
+        updatePlaybackRate(videoPlayer, items.number);
     });
 }
 
 if (typeof playbackValue !== 'undefined') {
-    updatePlaybackRate(playbackValue);
+    location.reload();
 }

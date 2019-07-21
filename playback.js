2fb77aaa056500b77f406c3abf33ee2cd3a729ca
Danny Fan <dannyfan@live.com>
2019-07-21 01:17:04 -0400

    Set slider values to change speed accordingly

--- playback.js
+++ playback.js
@@ -1,13 +1,30 @@
-function updatePlaybackRate() {
+function createVideoReference() {
+    if (document.querySelector('crunchroll-video-player-ref')) {
+        return false;
+    }
+    let s = document.createElement('script');
+    s.type = 'type/javascript';
+    s.id = 'crunchyroll-video-player-ref';
+    s.innerHTML = `var videoPlayerObj;`;
+    document.body.appendChild(s);
+}
+
+function updatePlaybackRate(playbackValue) {
     let videoPlayer = document.querySelector('#player_html5_api');
     if (videoPlayer !== null) {
         setTimeout(() => {
-            videoPlayer.playbackRate = 2;
+            console.log(playbackValue);
+            videoPlayer.playbackRate = playbackValue;
         }, 5000);
     } 
 }
 
 window.onload = function() {
-    updatePlaybackRate();
+    chrome.storage.sync.get({number: 2}, function(items) {
+        updatePlaybackRate(items.number);
+    });
 }
 
+if (typeof playbackValue !== 'undefined') {
+    updatePlaybackRate(playbackValue);
+}
--- range.js
+++ range.js
@@ -1,13 +1,34 @@
 const playbackRange = document.querySelector('#playrate-slider');
 const playbackValue = document.querySelector('#playrate-value');
+
 playbackRange.onchange = function(element) {
     let value = element.target.value;
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.executeScript(
+            tabs[0].id, {
+                code: 'var playbackValue = ' + value + ';'
+            }
+        ),
         chrome.tabs.executeScript(
             tabs[0].id, {
                 file: 'playback.js'
             }
         );
     });
+    saveValue(value);
     playbackValue.innerHTML = value;
 }
+
+function saveValue(value) {
+    chrome.storage.sync.set({number: value}, function() {
+    });
+}
+
+function getValue() {
+    chrome.storage.sync.get({number: 2}, function(items) {
+        playbackRange.value = items.number;
+        playbackValue.innerHTML = items.number;
+    });
+}
+
+document.addEventListener('DOMContentLoaded', getValue);

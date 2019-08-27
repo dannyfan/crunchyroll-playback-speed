83fb842d9defe82f9915e264e45d4d5b9c70c558
dannyfaninbox <dannyfaninbox@penguin>
2019-08-26 21:00:20 -0400

    Update extension to listen for keyboard shortcuts to change playback speed instead of using an ui slider

--- playback.js
+++ playback.js
@@ -1,18 +1,25 @@
-function updatePlaybackRate(videoPlayer, playbackValue) {
-    if (videoPlayer !== null) {
-        setTimeout(() => {
-            videoPlayer.playbackRate = playbackValue;
-        }, 5000);
-    } 
-}
-
 window.onload = function() {
-    let videoPlayer = document.querySelector('#player_html5_api');
-    chrome.storage.sync.get({number: 2}, function(items) {
-        updatePlaybackRate(videoPlayer, items.number);
+    document.addEventListener('keydown', function(e) {
+        e.preventDefault();
+        const videoPlayer = document.querySelector('#player_html5_api');
+        if (videoPlayer !== null) {
+            videoControls(videoPlayer, e);
+        }
     });
 }
 
-if (typeof playbackValue !== 'undefined') {
-    location.reload();
+function videoControls(videoPlayer, e) {
+    // Increase/decrease video player's playback speed
+    if (e.shiftKey && e.keyCode == 40 || e.shiftKey && e.keyCode == 38) {
+        let value = (e.keyCode == 40) ? -0.25 : 0.25;
+        let newPlaybackRate = videoPlayer.playbackRate + value;
+        videoPlayer.playbackRate = checkLimit(newPlaybackRate);
+    }
+}
+
+function checkLimit(playbackRate) {
+    // Limit the playback speed between 0.0 and 2.0
+    playbackRate = playbackRate < 0 ? 1.0 : playbackRate;
+    playbackRate = playbackRate > 2 ? 2.0 : playbackRate;
+    return playbackRate;
 }
